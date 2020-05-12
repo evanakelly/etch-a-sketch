7f36c3f56b781c2db2c22cc3a8ac26cb2bf6f01c
Evan Kelly <evanakelly@gmail.com>
2020-05-11 21:21:03 -0500

    Added greyscale and multicolor options using inline style rather than classes.

--- script.js
+++ script.js
@@ -4,35 +4,30 @@ function makeDivs() {
     const square = document.createElement('div');
     square.classList.add('square');
     container.appendChild(square);
-}
+};
 
 function makeBoard(boardSize) {
+    if (boardSize <1) {
+        boardSize = 1;
+    }
     for (i=0; i<(Math.pow(boardSize,2)); i++){
         makeDivs();
-    };
+    }
     let w = container.offsetWidth;
-    let sq = document.querySelectorAll('div.square');
-    sq.forEach((square) => {
+    let squares = document.querySelectorAll('div.square');
+    squares.forEach((square) => {
         square.style.width = ((w/boardSize)+'px');
         square.style.height = ((w/boardSize)+'px');
-        console.log(square.style.width);
-        console.log(square.style.height);
+    })
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.classList.add('on');
+        })
     })
 };
 
 let numStart = 16;
 makeBoard(numStart);
-
-function addOn() {
-    let squares = document.querySelectorAll('div.square');
-    squares.forEach((square) => {
-        square.addEventListener('mouseenter', () => {
-            square.classList.add('on');
-        });
-    });
-}
-
-addOn();
 
 const clearButton = document.querySelector('#clear-button');
 
@@ -40,9 +35,48 @@ clearButton.addEventListener("click", () => {
     let size = prompt("What width to redraw?", 16);
     document.querySelectorAll('div.square').forEach(function(x) {
         x.remove();
-    });
-
+    })
     makeBoard(size);
-    addOn();
-
 });
+
+const colorButton = document.querySelector('#color-button');
+
+function randColor() {
+    return `rgb(${Math.round(Math.random()*255)}, 
+    ${Math.round(Math.random()*255)},
+     ${Math.round(Math.random()*255)})`;
+};
+
+function colorIt() {
+    document.querySelectorAll('div.square').forEach(function(e) {
+        e.addEventListener('mouseover', () => {
+            e.style.backgroundColor = randColor();
+            e.style.opacity = '1.0';
+        })
+    })
+};
+
+colorButton.addEventListener('click', colorIt) 
+
+
+const greyButton = document.querySelector('#grey-button');
+
+let op = 0.0;
+
+greyButton.addEventListener('click', shadeIt);
+
+function shadeIt() {
+    document.querySelectorAll('div.square').forEach(function(e) {
+        //x.removeEventListener('mouseenter', );
+        let opIncrement = 0.1;
+        e.addEventListener('mouseover', () => {
+            e.style.backgroundColor = 'rgb(0,0,0)';
+            if (opIncrement <=0.9) {
+                opIncrement += 0.1;
+            }
+            e.style.opacity = `${op+opIncrement}`;
+        })
+    })
+};
+
+
